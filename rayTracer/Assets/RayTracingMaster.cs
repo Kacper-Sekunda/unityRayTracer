4f95153251c61eb7f2a7a6c1f31d22557571e029
Kacper-Sekunda <kacpersekunda@gmail.com>
2025-01-26 00:15:10 +0000

    rayTracer initial setup
    - Basic setup
    - Camera
    - Tracing
    - Added planes and spheres
    - Anti-aliasing
    - Reflections
    - Directional Light

---
+++ rayTracer/Assets/RayTracingMaster.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayTracingMaster : MonoBehaviour
+{
+    public ComputeShader RayTracingShader;
+    public Texture SkyboxTexture;
+
+    public Light DirectionalLight;
+    
+    private RenderTexture _target;
+    private Camera _camera;
+    
+    private uint _currentSample = 0;
+    private Material _addMaterial;
+
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+    }
+    
+    private void Update()
+    {
+        if (transform.hasChanged)
+        {
+            _currentSample = 0;
+            transform.hasChanged = false;
+        }
+        
+        Vector3 l = DirectionalLight.transform.forward;
+        RayTracingShader.SetVector("_DirectionalLight", new Vector4(l.x, l.y, l.z, DirectionalLight.intensity));
+    }
+
+    private void SetShaderParameters()
+    {
+        RayTracingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
+        RayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
+        RayTracingShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture);
+        RayTracingShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        SetShaderParameters();
+        Render(destination);
+    }
+
+    // OnRenderImage is auto called when camera has finished rendering
+    // Firstly we create a render target of appropriate dimensions
+    
+    private void Render(RenderTexture destination)
+    {
+        InitRenderTexture();
+        
+        RayTracingShader.SetTexture(0, "Result", _target);
+        int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
+        int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
+        RayTracingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+        
+        if (_addMaterial == null)
+            _addMaterial = new Material(Shader.Find("Hidden/AddShader"));
+        _addMaterial.SetFloat("_Sample", _currentSample);
+        Graphics.Blit(_target, destination, _addMaterial);
+        _currentSample++;
+    }
+
+    // Next the shader is dispatched
+    // Graphics.Blit writes result to screen
+    
+    private void InitRenderTexture()
+    {
+        if (_target == null || _target.width != Screen.width || _target.height != Screen.height)
+        {
+            // Release render texture if already have one
+            if (_target != null)
+                _target.Release();
+            
+            // Get render target for Ray Tracing
+            _target = new RenderTexture(Screen.width, Screen.height, 0,
+                RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _target.enableRandomWrite = true;
+            _target.Create();
+        }
+    }
+}
