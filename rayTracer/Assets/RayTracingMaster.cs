bdf6fb7ee2ed4c254461ae3aa579bb271c061e6a
Kacper-Sekunda <kacpersekunda@gmail.com>
2025-01-30 04:43:46 +0000

    Updated Tracer
    Main:
    - Specular
    - Diffuse
    General Improvements:
    - Added maxSampleCount
    - Added HUD
    - Added Controls
    - Screenshotting

--- rayTracer/Assets/RayTracingMaster.cs
+++ rayTracer/Assets/RayTracingMaster.cs
@@ -1,72 +1,239 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RayTracingMaster : MonoBehaviour
 {
+    [Header("Ray Tracing")]
     public ComputeShader RayTracingShader;
     public Texture SkyboxTexture;
-
     public Light DirectionalLight;
+
+    [Header("Debug")] 
+    public Text SeedCountText;
+    public Text SampleCountText;
     
-    private RenderTexture _target;
+    [Header("Samples")]
+    public int maxSampleCount = 8192;
+    
+    [Header("Spheres")]
+    public Vector2 SphereRadius = new Vector2(3.0f, 8.0f);
+    public uint SpheresMax = 100;
+    public float SpherePlacementRadius = 100.0f;
+    public int SphereSeed;
+    
+    // Sphere Buffer
+    private ComputeBuffer _sphereBuffer;
+        
+    // Camera
     private Camera _camera;
+    private float _lastFieldOfView;
     
-    private uint _currentSample = 0;
+    // Render Texture
+    private RenderTexture _target;
+    private RenderTexture _converged;
+    
+    // Material
     private Material _addMaterial;
+    
+    // Samples
+    private uint _currentSample = 0;
+    
+    // Other
+    private List<Transform> _transformsToWatch = new List<Transform>();
+    private int _lastSeed;
+    
+    struct Sphere
+    {
+        public Vector3 position;
+        public float radius;
+        public Vector3 albedo;
+        public Vector3 specular;
+        public float smoothness;
+        public Vector3 emission;
+    }
 
     private void Awake()
     {
         _camera = GetComponent<Camera>();
+        
+        _transformsToWatch.Add(transform);
+        _transformsToWatch.Add(DirectionalLight.transform);
     }
     
     private void Update()
     {
-        if (transform.hasChanged)
+        // Keybindings
+        
+        // Screenshot
+        if (Input.GetKeyDown(KeyCode.F12))
         {
-            _currentSample = 0;
-            transform.hasChanged = false;
+            // Disable
+            SeedCountText.enabled = false;
+            SampleCountText.enabled = false;
+            
+            // Screenshot 0-0
+            ScreenCapture.CaptureScreenshot( "Images/Seed_" + SphereSeed + " - sampleCount_" + _currentSample + ".png");
+            
+            // Reenable
+            StartCoroutine(RestoreText());
         }
         
-        Vector3 l = DirectionalLight.transform.forward;
-        RayTracingShader.SetVector("_DirectionalLight", new Vector4(l.x, l.y, l.z, DirectionalLight.intensity));
+        // Change Seed
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.D))
+        {
+            SphereSeed++;
+        }else if (Input.GetKeyDown(KeyCode.D) && !Input.GetKey(KeyCode.LeftShift))
+        {
+            SphereSeed++;
+        }else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.A))
+        {
+            SphereSeed--;
+        }else if (Input.GetKeyDown(KeyCode.A) && !Input.GetKey(KeyCode.LeftShift))
+        {
+            SphereSeed--;
+        }
+
+        // Change fov
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
+        {
+            _camera.fieldOfView--;
+        }else if (Input.GetKeyDown(KeyCode.W) && !Input.GetKey(KeyCode.LeftShift))
+        {
+            _camera.fieldOfView--;
+        }else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.S))
+        {
+            _camera.fieldOfView++;
+        }else if (Input.GetKeyDown(KeyCode.S) && !Input.GetKey(KeyCode.LeftShift))
+        {
+            _camera.fieldOfView++;
+        }
+
+        // Updates fov
+        if (_camera.fieldOfView != _lastFieldOfView)
+        {
+            _currentSample = 0;
+            _lastFieldOfView = _camera.fieldOfView;
+        }
+        
+        // Updates transform
+        foreach (Transform t in _transformsToWatch)
+        {
+            if (t.hasChanged)
+            {
+                _currentSample = 0;
+                t.hasChanged = false;
+            }
+        }
+        
+        // Seed -----------------------------------------------------------
+        // Updates seed
+        if (_lastSeed != SphereSeed)
+        {
+            _lastSeed = SphereSeed;     // Update lastSeed
+            _currentSample = 0;         // Reset Samples
+            SetUpScene();               // Regenerate spheres with new seed
+        }
+        
+        // Updates text
+        if (SeedCountText != null)
+        {
+            SeedCountText.text = "Seed = " + SphereSeed.ToString();
+        }
+        // ----------------------------------------------------------------
+    }
+
+    private IEnumerator RestoreText()
+    {
+        yield return new WaitForSeconds(0.05f);
+        SeedCountText.enabled = true;
+        SampleCountText.enabled = true;
+    }
+    
+    private void OnEnable()
+    {
+        _currentSample = 0;
+        SetUpScene();
+    }
+    
+    private void OnDisable()
+    {
+        if (_sphereBuffer != null)
+            _sphereBuffer.Release();
+    }
+
+    private void SetUpScene()
+    {
+        Random.InitState(SphereSeed);  // Reinitialize the random state based on the new seed
+        List<Sphere> spheres = new List<Sphere>();
+
+        // Add a number of random spheres
+        for (int i = 0; i < SpheresMax; i++)
+        {
+            Sphere sphere = new Sphere();
+            // Randomly choose radius
+            sphere.radius = SphereRadius.x + Random.value * (SphereRadius.y - SphereRadius.x);
+
+            // Randomly place spheres within a defined radius
+            Vector2 randomPos = Random.insideUnitCircle * SpherePlacementRadius;
+            sphere.position = new Vector3(randomPos.x, sphere.radius, randomPos.y);
+
+            // Reject spheres that are intersecting others
+            foreach (Sphere other in spheres)
+            {
+                float minDist = sphere.radius + other.radius;
+                if (Vector3.SqrMagnitude(sphere.position - other.position) < minDist * minDist)
+                    goto SkipSphere;
+            }
+
+            // Albedo and specular color
+            Color color = Random.ColorHSV();
+            float chance = Random.value;
+            if (chance < 0.9f)
+            {
+                bool metal = chance < 0.4f;
+                sphere.albedo = metal ? Vector4.zero : new Vector4(color.r, color.g, color.b);
+                sphere.specular = metal ? new Vector4(color.r, color.g, color.b) : new Vector4(0.04f, 0.04f, 0.04f);
+                sphere.smoothness = Random.value;
+            }
+            else
+            {
+                Color emission = Random.ColorHSV(0, 1, 0, 1, 3.0f, 8.0f);
+                sphere.emission = new Vector3(emission.r, emission.g, emission.b);
+            }
+            // Add the sphere to the list
+            spheres.Add(sphere);
+
+            SkipSphere:
+            continue;
+        }
+
+        // Assign to compute buffer
+        if (_sphereBuffer != null)
+            _sphereBuffer.Release();
+        if (spheres.Count > 0)
+        {
+            _sphereBuffer = new ComputeBuffer(spheres.Count, 56);
+            _sphereBuffer.SetData(spheres);
+        }
     }
 
     private void SetShaderParameters()
     {
+        RayTracingShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture);
         RayTracingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
         RayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
-        RayTracingShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture);
         RayTracingShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
-    }
+        RayTracingShader.SetFloat("_Seed", Random.value);
 
-    private void OnRenderImage(RenderTexture source, RenderTexture destination)
-    {
-        SetShaderParameters();
-        Render(destination);
-    }
+        //Vector3 l = DirectionalLight.transform.forward;
+        //RayTracingShader.SetVector("_DirectionalLight", new Vector4(l.x, l.y, l.z, DirectionalLight.intensity));
 
-    // OnRenderImage is auto called when camera has finished rendering
-    // Firstly we create a render target of appropriate dimensions
-    
-    private void Render(RenderTexture destination)
-    {
-        InitRenderTexture();
-        
-        RayTracingShader.SetTexture(0, "Result", _target);
-        int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
-        int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
-        RayTracingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
-        
-        if (_addMaterial == null)
-            _addMaterial = new Material(Shader.Find("Hidden/AddShader"));
-        _addMaterial.SetFloat("_Sample", _currentSample);
-        Graphics.Blit(_target, destination, _addMaterial);
-        _currentSample++;
+        if (_sphereBuffer != null)
+            RayTracingShader.SetBuffer(0, "_Spheres", _sphereBuffer);
     }
-
-    // Next the shader is dispatched
-    // Graphics.Blit writes result to screen
     
     private void InitRenderTexture()
     {
@@ -74,13 +241,63 @@ public class RayTracingMaster : MonoBehaviour
         {
             // Release render texture if already have one
             if (_target != null)
+            {
                 _target.Release();
-            
+                _converged.Release();
+            }
+
             // Get render target for Ray Tracing
             _target = new RenderTexture(Screen.width, Screen.height, 0,
                 RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
             _target.enableRandomWrite = true;
             _target.Create();
+            _converged = new RenderTexture(Screen.width, Screen.height, 0,
+                RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _converged.enableRandomWrite = true;
+            _converged.Create();
+
+            // Reset sampling
+            _currentSample = 0;
         }
+    }
+    
+    private void Render(RenderTexture destination)
+    {
+        // Make sure we have current render target
+        InitRenderTexture();
+
+        // Set target and dispatch compute shader
+        RayTracingShader.SetTexture(0, "Result", _target);
+        int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
+        int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
+        RayTracingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+
+        // Blit result texture to screen
+        if (_addMaterial == null)
+            _addMaterial = new Material(Shader.Find("Hidden/AddShader"));
+        _addMaterial.SetFloat("_Sample", _currentSample);
+        Graphics.Blit(_target, _converged, _addMaterial);
+        Graphics.Blit(_converged, destination);
+        if (_currentSample < maxSampleCount)
+        {
+            _currentSample++;
+            if (SampleCountText != null)
+            {
+                SampleCountText.text = "Samples = " + _currentSample.ToString();
+            }
+        }
+        else
+        {
+            if (SampleCountText != null)
+            {
+                 SampleCountText.text = "Samples = 8192";
+            }
+        }
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        SetShaderParameters();
+        Render(destination);
     }
 }
